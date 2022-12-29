b359bda1f257ecbd26b353ea3d4a4ca7cbc9fd70
Jawad Iqbal <>
2022-12-29 14:18:48 +0100

    cardview amends

--- app/src/main/java/com/dreamlogix/ecomecompose/MainActivity.kt
+++ app/src/main/java/com/dreamlogix/ecomecompose/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dreamlogix.ecomecompose.uiElements.CardView
 
@@ -37,11 +38,9 @@ CardView()
     }
 
 
-//    @Preview(showBackground = true)
-//    @Composable
-//    fun DefaultPreview() {
-//        EcomeComposeTheme {
-////            gerrting()
-//        }
-//    }
-//}
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+      CardView()
+
+    }
--- app/src/main/java/com/dreamlogix/ecomecompose/uiElements/CardView.kt
+++ app/src/main/java/com/dreamlogix/ecomecompose/uiElements/CardView.kt
@@ -1,53 +1,51 @@
 package com.dreamlogix.ecomecompose.uiElements
 
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import java.util.Locale
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CardView(){
-
+fun CardView() {
     Card(
         modifier = Modifier
+            .fillMaxWidth().fillMaxHeight()
+            .padding(5.dp),colors = CardDefaults.cardColors(
+            containerColor =  MaterialTheme.colorScheme.primary),elevation = CardDefaults.cardElevation(
+            defaultElevation =  20.dp,
+        ),
+        shape = RoundedCornerShape(10.dp),border = BorderStroke(2.dp, androidx.compose.ui.graphics.Color.Black), onClick = {
 
-            .padding(15.dp)
-            .clickable{ },
+        },
 
-    ) {
-        Column(
-            modifier = Modifier.padding(15.dp)
-        ) {
-            Text(
-                buildAnnotatedString {
-                    append("welcome to ")
-                    withStyle(style = SpanStyle(fontWeight = FontWeight.W900)
-                    ) {
-                        append("Jetpack Compose Playground")
-                    }
-                }
-            )
-            Text(
-                buildAnnotatedString {
-                    append("Now you are in the ")
-                    withStyle(style = SpanStyle(fontWeight = FontWeight.W900)) {
-                        append("Card")
-                    }
-                    append(" section")
-                }
-            )
+        content = {
+            Text("Card with content argument",
+                modifier = Modifier.padding(16.dp),
+                style = MaterialTheme.typography.labelLarge)
         }
-    }
+    )
+}
+fun toast(){
+    val context: Context? = null
+    Toast.makeText(context,"this",Toast.LENGTH_SHORT).show()
 
 }
+
+
+
+
